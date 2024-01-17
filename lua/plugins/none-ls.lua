11938c59e2e6a48b0e6bb44df40bc61db5536938
Lonitch <invictumltd@gmail.com>
2024-01-16 19:47:44 -0600

    use eslint in none-ls.lua

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,13 +3,14 @@ return {
   config= function()
     local null_ls = require("null-ls")
     null_ls.setup({
+      debug = true,
       sources = {
         -- auto-format lua
         null_ls.builtins.formatting.stylua,
         -- js formatting
         null_ls.builtins.formatting.prettier,
         -- eslint diagnostics
-        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.eslint,
         -- spell completion
         null_ls.builtins.completion.spell,
       }
