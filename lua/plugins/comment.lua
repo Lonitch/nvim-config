6f2a9b076c2e5cf9db1c8b28058c332ec05a6b5c
Lonitch <invictumltd@gmail.com>
2024-02-09 14:58:31 -0600

    add ts-context-commentstring

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,20 @@
 return {
-    'numToStr/Comment.nvim',
-    opts = {
-    },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
     lazy = false,
-    config = function ()
-      require('Comment').setup()
-    end
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
 }
-
