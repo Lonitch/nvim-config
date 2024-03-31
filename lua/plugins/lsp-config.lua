dcdfe3a196457bda477c5ad55e3611dcb904f3b9
Lonitch <invictumltd@gmail.com>
2024-03-30 22:19:23 -0500

    quiet pyright a bit

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,63 +1,71 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        -- define LSP servers here for LUA, RUST, JS/TS, and PYTHON
-        ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "pyright" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- Capabilities required for the visualstudio lsps (css, html, etc)
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-      local lspconfig = require("lspconfig")
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-        filetypes = { "rust" },
-        settings = {
-          ["rust-analyzer"] = {
-            cargo = { allFeatures = true },
-            procMacro = {
-              ignored = {
-                leptos_macro = {
-                  -- optional: --
-                  "component",
-                  "server",
-                },
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- define LSP servers here for LUA, RUST, JS/TS, and PYTHON
+				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- Capabilities required for the visualstudio lsps (css, html, etc)
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
+			local lspconfig = require("lspconfig")
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "off",
+							autoSearchPaths = false,
+							useLibraryCodeForTypes = false,
+						},
+					},
+				},
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				filetypes = { "rust" },
+				settings = {
+					["rust-analyzer"] = {
+						cargo = { allFeatures = true },
+						procMacro = {
+							ignored = {
+								leptos_macro = {
+									-- optional: --
+									"component",
+									"server",
+								},
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
 }
