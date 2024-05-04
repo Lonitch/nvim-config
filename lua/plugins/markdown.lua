a65585832e6e13fef71d4e1907b06a58bfbaa5a7
Lonitch <invictumltd@gmail.com>
2024-05-03 19:01:04 -0500

    fix tabular issue with Mtoc

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
+  { "godlygeek/tabular" },
   {
-    "godlygeek/tabular",
     "hedyhli/markdown-toc.nvim",
     ft = "markdown", -- Lazy load on markdown filetype
     cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
